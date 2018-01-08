57317ba7a46ce83ef18585a414ae4b2ac042787f
Stephen <jeffrypig23@gmail.com>
2018-01-08 13:28:23 -0800

    Updated main window, and made a popup for team numbers

--- app/src/main/java/com/example/stephenogden/a1595scoutingapp/MainActivity.java
+++ app/src/main/java/com/example/stephenogden/a1595scoutingapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.stephenogden.a1595scoutingapp;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -15,15 +20,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    // <Keep>
+    final Context context = this;
+    public Button start_button;
+    public EditText result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // </Keep>
 
         Button settings_btn = (Button)findViewById(R.id.settings);
-
         settings_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,15 +37,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        Button start_btn = (Button)findViewById(R.id.start);
-
-        start_btn.setOnClickListener(new View.OnClickListener() {
+        start_button = (Button)findViewById(R.id.start);
+        start_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, scouting.class));
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.teamselection, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                // set prompts.xml to alertdialog builder
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView.findViewById(R.id.editTextDialogUserInput);
+
+                // set dialog message
+                alertDialogBuilder.setCancelable(false).setPositiveButton("Start scouting!", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        // get user input and set it to result
+                        // edit text
+                        result.setText(userInput.getText());
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
             }
         });
-
-
     }
 }
