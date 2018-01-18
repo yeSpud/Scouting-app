884919e73d6c30d58bdcb3cb3110bc71bf66ef0b
Stephen <jeffrypig23@gmail.com>
2018-01-18 00:10:47 -0800

    Added xCode files for iOS development

---
+++ iOS/1595 Scouting App/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  1595 Scouting App
+//
+//  Created by Stephen Ogden on 1/16/18.
+//  Copyright © 2018 1595 Dragons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	var teamNumber = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+		
+    }
+	func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+		if (segue.identifier == "toSettings") {
+			let abcViewController = segue.destination
+			abcViewController.title = "Main"
+		} else if (segue.identifier == "toMainPage") {
+			let abcViewController = segue.destination
+			abcViewController.title = "Settings"
+		}
+	}
+	
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	func showScoutingDialog() {
+		//Creating UIAlertController and
+		//Setting title and message for the alert dialog
+		let alertController = UIAlertController(title: "Enter team number", message: "Enter the team number to start scouting", preferredStyle: .alert)
+		//the confirm action taking the inputs
+		let confirmAction = UIAlertAction(title: "Enter", style: .default) { (_) in
+			//getting the input values from user
+			alertController.addTextField(configurationHandler: { textField in
+				textField.keyboardType = .numberPad
+			})
+			self.teamNumber =  Int((alertController.textFields?[0].text)!)!
+
+		}
+		//the cancel action doing nothing
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { (_) in }
+		//adding textfields to our dialog box
+		alertController.addTextField { (textField) in
+			textField.placeholder = "Enter team number"
+		}
+		//adding the action to dialogbox
+		alertController.addAction(confirmAction)
+		alertController.addAction(cancelAction)
+		//finally presenting the dialog box
+		self.present(alertController, animated: true, completion: nil)
+	}
+
+    @IBAction func startButtonClicked() {
+        showScoutingDialog()
+    }
+    @IBAction func settingsButtonClicked() {
+        performSegue(withIdentifier: "toSettings", sender: nil)
+    }
+	
+	@IBAction func backButtonClicked() {
+		performSegue(withIdentifier: "toMainPage", sender: nil)
+	}
+	
+}
+
