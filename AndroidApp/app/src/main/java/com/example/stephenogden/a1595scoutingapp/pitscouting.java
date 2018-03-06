0979ff3de593b77b5cf62dd790a2582b6dab3d35
Stephen <jeffrypig23@gmail.com>
2018-03-06 14:48:01 -0800

    Added font used by team

--- AndroidApp/app/src/main/java/com/example/stephenogden/a1595scoutingapp/pitscouting.java
+++ AndroidApp/app/src/main/java/com/example/stephenogden/a1595scoutingapp/pitscouting.java
@@ -1,6 +1,11 @@
 package com.example.stephenogden.a1595scoutingapp;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Stephen Ogden on 3/6/18.
@@ -10,5 +15,11 @@ import android.support.v7.app.AppCompatActivity;
 
 public class pitscouting extends AppCompatActivity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pitscouting);
 
+
+        }
 }
