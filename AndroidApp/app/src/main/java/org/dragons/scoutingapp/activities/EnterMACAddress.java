8c7fabeb5cb8489c628d3e65f3e98bb181bb06b5
Spud <jeffrypig23@gmail.com>
2021-09-15 16:53:37 -0700

    Added QR code scanner

--- AndroidApp/app/src/main/java/org/dragons/scoutingapp/activities/EnterMACAddress.java
+++ AndroidApp/app/src/main/java/org/dragons/scoutingapp/activities/EnterMACAddress.java
@@ -1,19 +1,29 @@
 package org.dragons.scoutingapp.activities;
 
+import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Process;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import com.google.zxing.BarcodeFormat;
 import com.google.zxing.Result;
 
+import org.dragons.scoutingapp.BuildConfig;
 import org.dragons.scoutingapp.R;
 import org.dragons.scoutingapp.bluefiles.BlueThread;
 import org.dragons.scoutingapp.databinding.MacaddressBinding;
+
+import java.util.Collections;
+import java.util.Locale;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -23,58 +33,96 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
  */
 public class EnterMACAddress extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
+	/**
+	 * Documentation
+	 */
+	private MacaddressBinding binder;
+
+	/**
+	 * Documentation
+	 */
 	private Animation shake;
 
 	@Override
 	protected void onCreate(@Nullable Bundle savedInstance) {
 		super.onCreate(savedInstance);
 
-		final MacaddressBinding binder = DataBindingUtil.setContentView(this, R.layout.macaddress);
+		this.binder = DataBindingUtil.setContentView(this, R.layout.macaddress);
 		this.setTitle(R.string.enter_mac_address);
+
 		this.shake = AnimationUtils.loadAnimation(this, R.anim.skake);
 
-
-		binder.debugConnected.setOnClickListener(view -> {
+		this.binder.debugConnected.setOnClickListener(view -> {
 			BlueThread.INSTANCE.start(this.getResources().getString(R.string.debug_connected), this);
 			this.finish();
 		});
 
-		binder.debugMatchData.setOnClickListener(view -> {
+		this.binder.debugMatchData.setOnClickListener(view -> {
 			BlueThread.INSTANCE.start(this.getResources().getString(R.string.debug_match_data), this);
 			this.finish();
 		});
 
-		binder.connect.setOnClickListener(view -> {
-			String macAddress = binder.macAddressInput.getText().toString().toUpperCase(java.util.Locale.US);
+		this.binder.connect.setOnClickListener(view -> {
+			String macAddress = this.binder.macAddressInput.getText().toString().toUpperCase(Locale.US);
 
 			if (BluetoothAdapter.checkBluetoothAddress(macAddress)) {
 				BlueThread.INSTANCE.start(macAddress, this);
 				this.finish();
 			} else {
-				binder.macAddressInput.startAnimation(this.shake);
+				this.binder.macAddressInput.startAnimation(this.shake);
 			}
 		});
 
-		binder.scan.setOnClickListener(view -> {
+		this.binder.scan.setOnClickListener(view -> {
 
-			// TODO Test for camera permissions
-
-			ZXingScannerView scannerView = new ZXingScannerView(this);
-			scannerView.setResultHandler(this);
-			scannerView.startCamera();
+			if (this.checkPermission(Manifest.permission.CAMERA, Process.myPid(), Process.myUid())
+					== PackageManager.PERMISSION_DENIED) {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+					this.requestPermissions(new String[]{Manifest.permission.CAMERA}, this.hashCode());
+				}
+			} else {
+				this.startQRCodeCamera();
+			}
 		});
 
-		binder.cancel.setOnClickListener(view -> this.finish());
+		this.binder.cancel.setOnClickListener(view -> this.finish());
+	}
+
+	/**
+	 * Documentation
+	 */
+	private void startQRCodeCamera() {
+		this.binder.cameraView.setResultHandler(this);
+		this.binder.cameraView.startCamera();
+		this.binder.cameraView.setAutoFocus(true);
+		this.binder.cameraView.setFormats(Collections.singletonList(BarcodeFormat.QR_CODE));
 	}
 
 	@Override
 	protected void onPause() {
 		super.onPause();
+		this.binder.cameraView.stopCameraPreview();
+		this.binder.cameraView.stopCamera();
 		this.finish();
 	}
 
 	@Override
-	public void handleResult(Result result) {
-		// TODO
+	public void onRequestPermissionsResult (int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+		if (requestCode == this.hashCode()) {
+			if (permissions[0].equals(Manifest.permission.CAMERA)) {
+				if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+					this.startQRCodeCamera();
+				}
+			}
+		}
+	}
+
+	@Override
+	public void handleResult(@NonNull Result result) {
+		this.binder.cameraView.stopCameraPreview();
+		this.binder.cameraView.stopCamera();
+		this.binder.macAddressInput.setText(result.getText());
 	}
 }
